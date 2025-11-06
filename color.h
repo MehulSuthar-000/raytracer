90431c14939d25501545bda297d2c57191087112
Mehul Suthar <mehulsuthar858@gmail.com>
2025-11-06 23:56:04 +0530

    implementing Anti Aliasing

--- color.h
+++ color.h
@@ -1,8 +1,8 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "interval.h"
 #include "vec3.h"
-#include <iostream>
 
 // Clamp helper to keep values in [min,max]
 inline double clamp(double x, double min, double max) {
@@ -18,15 +18,11 @@ void write_color(std::ostream& out, const color& pixel_color) {
     auto g = pixel_color.y();
     auto b = pixel_color.z();
 
-    // Clamp to [0,1) to avoid overflow.
-    r = clamp(r, 0.0, 0.999);
-    g = clamp(g, 0.0, 0.999);
-    b = clamp(b, 0.0, 0.999);
-
-    // Translate the [0,1) range to [0,255]
-    int rbyte = static_cast<int>(255.999 * r);
-    int gbyte = static_cast<int>(255.999 * g);
-    int bbyte = static_cast<int>(255.999 * b);
+    // Translate the [0,1] component values to the bytes range [0, 255].
+    static const interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
 
     // Write the color
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
