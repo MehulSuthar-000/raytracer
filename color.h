73deccdf43f431ab79cec78db246a98144c0651a
Mehul Suthar <mehulsuthar858@gmail.com>
2025-11-07 21:15:05 +0530

    Final implementation

--- color.h
+++ color.h
@@ -11,12 +11,25 @@ inline double clamp(double x, double min, double max) {
     return x;
 }
 
+inline double linear_to_gamma(double linear_component) {
+    if (linear_component > 0) {
+        return std::sqrt(linear_component);
+    }
+
+    return 0;
+}
+
 using color = vec3;
 
 void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    // Apply a linear to gamma transform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Translate the [0,1] component values to the bytes range [0, 255].
     static const interval intensity(0.000, 0.999);
